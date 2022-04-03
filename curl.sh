b498442d175d8cc1002b8c795bb10fa44d4ff555
bartr <bartr@microsoft.com>
2022-04-03 17:05:35 -0500

    added curl.sh

---
+++ curl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# curl heartbeat
+curl https://central-il-chi-101.cseretail.com/heartbeat/17; echo "   central-il-chi-101" &
+curl https://central-il-chi-102.cseretail.com/heartbeat/17; echo "   central-il-chi-102" &
+curl https://east-ny-nyc-101.cseretail.com/heartbeat/17; echo "   east-ny-nyc-101" &
+curl https://east-ny-nyc-102.cseretail.com/heartbeat/17; echo "   east-ny-nyc-102" &
+curl https://west-or-pdx-101.cseretail.com/heartbeat/17; echo "   west-or-pdx-101" &
+curl https://west-or-pdx-102.cseretail.com/heartbeat/17; echo "   west-or-pdx-102" &
+
+# curl the app
+curl https://central-il-chi-101.cseretail.com/api/benchmark/18; echo "  central-il-chi-101" &
+curl https://central-il-chi-102.cseretail.com/api/benchmark/18; echo "  central-il-chi-102" &
+
+#curl https://east-ny-nyc-101.cseretail.com/api/benchmark/18; echo "  east-ny-nyc-101" &
+#curl https://east-ny-nyc-102.cseretail.com/api/benchmark/18; echo "  east-ny-nyc-102" &
+
+#curl https://west-or-pdx-101.cseretail.com/api/benchmark/18; echo "  west-or-pdx-101" &
+#curl https://west-or-pdx-102.cseretail.com/api/benchmark/18; echo "  west-or-pdx-102" &
